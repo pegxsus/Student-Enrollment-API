26b6aac5a6e26792afeaa92274065eab0ee11bad
Paul Abraham <111341602+Pegasusx10@users.noreply.github.com>
2022-10-18 19:11:09 +0530

    Made changes to code arrangement as per comments

--- server.js
+++ server.js
@@ -4,16 +4,17 @@ const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 const student = require('./api/student')
+const studentRouter = require('./routes/students')
+const db = mongoose.connection
+const port = 5000
 
 mongoose.connect('mongodb+srv://Pegasusx10:pegasus123@cluster0.749smlf.mongodb.net/student-database?retryWrites=true&w=majority', { useUnifiedTopology: true, useNewUrlParser: true })
-const db = mongoose.connection
+
 db.on('error', (error) => console.error(error))
-// db.once('open', () => console.log('Connected to Database'))
 
 app.use(express.json())
 
-const studentsRouter = require('./routes/students')
-app.use('/api/students', studentsRouter)
-app.use('/api/student', studentsRouter)
+app.use('/api/students', studentRouter)
+app.use('/api/student', studentRouter)
 
-app.listen(5000, () => console.log('Server Started'))
+app.listen(port, () => console.log('Server Started'))
