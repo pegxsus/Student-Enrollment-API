0c09515157227f600776b71120eba8f2df0f1403
Paul Abraham <111341602+Pegasusx10@users.noreply.github.com>
2022-11-03 14:51:05 +0530

    Moved Mongo DB credentials to a different File

--- server.js
+++ server.js
@@ -4,24 +4,11 @@ const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 const studentRouter = require('./routes/students')
+const connection = require('./config/dbconnection')
 const db = mongoose.connection
-const port = 5000
+const PORT = 5000
 const dotenv = require("dotenv")
-const uri = process.env.MONGODB_CONNECTION_STRING
 dotenv.config()
-
-mongoose.connect(uri, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useUnifiedTopology: true, 
-})
-
-const connection = mongoose.connection;
-connection.once("open", () => {
-    console.log("MongoDB database connection established successfully.")
-})
-
-
 db.on('error', (error) => console.error(error))
 
 app.use(express.json())
@@ -29,4 +16,4 @@ app.use(express.json())
 app.use('/api/students', studentRouter)
 app.use('/api/student', studentRouter)
 
-app.listen(port, () => console.log('Server Started'))
+app.listen(PORT, () => console.log('Server Started'))
