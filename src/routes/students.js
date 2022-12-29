01dd10662edf6d2a9445a1d41c40e8cc8f9d6a9d
Paul Abraham <111341602+Pegasusx10@users.noreply.github.com>
2022-12-29 19:24:20 +0530

    Update: added feature to validate firstname & lastname

--- src/routes/students.js
+++ src/routes/students.js
@@ -2,11 +2,11 @@ const express = require('express')
 const router = express.Router()
 const student = require('../models/student')
 const queryCondition = require('../utils/logic')
-
+// const { getData, createData, updateData, deleteData } = require('../controller/students');
 
 
 // Getting all Pagination and Query
-router.get('/', async (req, res, next) => {
+router.get('/students', async (req, res, next) => {
   try {
     const pageSize = parseInt(req.query.pageSize) || 0
     const pageNumber = parseInt(req.query.pageNumber) || 1 
@@ -20,44 +20,43 @@ router.get('/', async (req, res, next) => {
     } catch (err) {
       next(err)
     }
-    })
+})
     
 // Getting a single student
 router.get('/:id', async (req, res) => {
   try{
-  const studentInfo = await student.findById(req.params.id)
+  const studentInfo = await student.findById(req.params.id).populate('enrolledCourses')
    res.send(studentInfo)
   } catch (err) {
     res.status(401).json(`Student database not found!`)
   }
- }) 
+}) 
 
 // Creating one with array 
-router.post('/', async (req, res, next) => {
+router.post('/students', async (req, res, next) => {
   const newStudent = new student({
     firstName: req.body.firstName,
     lastName: req.body.lastName,
     grade: req.body.grade,
     division: req.body.division,
     enrolledCourses: req.body.enrolledCourses
-  })
-  
-// Validate with Unique names
-  // const firstName = req.body.firstName;
-  // const lastName = req.body.lastName;
+})
 
-  // if (!firstName || !lastName) {
-  //   return res.status(400).send({ error: 'First and last name are required' });
-  // }
+// Validate firstName & lastName to be unique
+  const firstName = req.body.firstName;
+  const lastName = req.body.lastName;
+  student.findOne({ firstName: firstName, lastName: lastName }, (err, student) => {
+    if (err) {
+      return;
+    }
+    if (student) {
+      res.send({ isUnique: false });
+    } else {
+      res.send({ isUnique: true });
+    }
+});
 
-  // const uniqueFirstName = [...new Set(Array.from(firstName))];
-  // const uniqueLastName = [...new Set(Array.from(lastName))];
-
-  // if (uniqueFirstName.length < firstName.length || uniqueLastName.length < lastName.length) {
-  //   return res.status(400).send({ error: 'First and last name must be unique' });
-  // }
-
-// Validate courses 4 
+// Validate enrolled courses to 4 
   const enrolledCourses = req.body.enrolledCourses;
   if (!Array.isArray(enrolledCourses)) {
     return res.status(400).send({ error: 'Enrolled courses must be an array' });
@@ -68,9 +67,8 @@ router.post('/', async (req, res, next) => {
   }
 
   try {
-    const freshstudent = await newStudent
+    const freshstudent = await newStudent.save()
     res.status(201).json(freshstudent)
-    .save()
     } catch (err) {
       next(err)
     }
@@ -94,7 +92,7 @@ router.delete('/:id', async (req, res, next) => {
   try {
     const studentId = await student.findByIdAndDelete(req.params.id)
     res.send(`The student has been deleted has been successfully deleted!`)
-  } catch (e) {
+  } catch (err) {
     next(err)
   }
 })
