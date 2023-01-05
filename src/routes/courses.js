4c4e0c603eaad9c96dc814ca21b0af8f3688bc71
Paul Abraham <111341602+Pegasusx10@users.noreply.github.com>
2023-01-05 12:04:29 +0530

    moved async await functions to controllers folder

--- src/routes/courses.js
+++ src/routes/courses.js
@@ -1,72 +1,14 @@
 
 const express = require('express')
-const { set } = require('mongoose')
 const router = express.Router()
-const courses = require('../models/courses')
+const coursesController = require('../controllers/students');
 
-// Getting all 
-router.get('/', async (req, res) => {
-  try {
-    const students = await courses.find()
-    res.json(students)
-      } catch (err) {
-        res.status(401).json(`The input enpoint URL is not valid!`)
-      }
-    })
-    
-// Getting a single course
-router.get('/courses/:id', async (req, res) => {
-  try{
-  const coursesInfo = await courses.findById(req.params.id)
-   res.send(coursesInfo)
-  } catch (err) {
-    res.status(401).json(`courses database not found!`)
-  }
- }) 
 
-// Creating one
-router.post('/', async (req, res) => {
-  const newCourses = new courses({
-    title: req.body.title,
-    isElective: req.body.isElective,
-    teacher: req.body.teacher
-  })
-  //  Unique Courses Field
-  function isUnique(arr){
-    return new set(arr).size === arr.length;
-  }
-  if (!isUnique(title)) {
-    return res.status(400).send({ error: 'Entered course should be unique'})
-  }
-  try {
-    const freshCourses = await newCourses.save()
-    res.status(201).json(freshCourses)
-  } catch (err) {
-    res.status(401).json(`Courses database not found!`)
-  }
-})
+router.get('/courses', coursesController.getCourses);
+router.get('/courses/:id', coursesController.getCourses);
+router.post('/courses', coursesController.postCourses);
+router.put('/courses/:id', coursesController.updateCourses);
+router.delete('/courses/:id', coursesController.deleteCourses);
 
-// Updating One
-router.put('/courses/:id', async (req, res, next) => {
-  courses.findByIdAndUpdate({_id: req.params.id}, req.body).then(function(){
-    courses.findOne({_id: req.params.id}).then(function(courses){
-    res.send(`Courses with ID ${courses._id} has been Updated.`)
-  })
-})
-  .catch(err=>{
-    res.status(401).json({error:'The input URL is incorrect!' })
-  })
-})
-
-//Delete by ID Method
-router.delete('/courses/:id', async (req, res, next) => {
-  try {
-      const coursesId = await courses.findByIdAndDelete(req.params.id)
-      res.send(`The Course ${coursesId.title} has been deleted from the database`)
-  }
-  catch (error) {
-      res.status(401).json(`Courses database not found!`)
-  }
-})
 
 module.exports = router
