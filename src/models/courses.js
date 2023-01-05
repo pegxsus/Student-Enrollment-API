2fdcae176792818b3923bfce23bb69206f5738a1
Paul Abraham <111341602+Pegasusx10@users.noreply.github.com>
2023-01-05 15:29:48 +0530

    Validation logic moved to models folder

--- src/models/courses.js
+++ src/models/courses.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose')
 const coursesSchema = new mongoose.Schema({
   title: {
     type: String,
-    required: true
+    required: true,
+    unique: true
   },
   isElective: {
     type: Boolean,
@@ -14,5 +15,20 @@ const coursesSchema = new mongoose.Schema({
     required: true
   },
 })
+
+// Custom validation function to check for unique title
+coursesSchema.path('title').validate(function(value) {
+  return new Promise((resolve, reject) => {
+    this.constructor.findOne({ title: value }, (err, course) => {
+      if (err) reject(err)
+      if (course) {
+        if (this.id === courses.id) resolve(true)
+        else reject(new Error('Title must be unique'))
+      } else {
+        resolve(true)
+      }
+    })
+  })
+}, 'Title must be unique')
 
 module.exports = mongoose.model('courses', coursesSchema)
