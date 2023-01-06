bcde5b2104a8530a263569183c556b923746dc01
Paul Abraham <111341602+Pegasusx10@users.noreply.github.com>
2023-01-06 13:14:46 +0530

    Removed commented-out lines of code

--- server.js
+++ server.js
@@ -5,26 +5,23 @@ const app = express()
 const mongoose = require('mongoose')
 const studentRouter = require('./src/routes/students')
 const coursesRouter = require('./src/routes/courses')
-const connection = require('./controller/dbConnection')
+const connection = require('./src/config/dbConnection')
 const db = mongoose.connection
 const PORT = 5000
 const dotenv = require("dotenv")
-
 
 dotenv.config()
 
 db.on('error', (error) => console.error(error))
 
 app.use(express.json())
-app.use('/api/students', studentRouter)
-app.use('/api/student', studentRouter)
-app.use('/api/courses', coursesRouter)
+app.use('/api', coursesRouter)
+app.use('/api', studentRouter)
 
 app.use('*', (req,res) => {
     const err = new Error(`Requested URL is Invalid!`)
     res.status(404).json({        
-        message: err.message
-        // log: err.stack
+        message: err.message       
     })
 })
 
@@ -32,7 +29,6 @@ app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500
     res.status(statusCode).json({        
         message: err.message
-        // log: err.stack
     })
 })
 
